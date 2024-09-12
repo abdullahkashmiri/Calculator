9e63e53a601a7a3d0903ee6a9fec78aa4d60529c
abdullah.dar <darabdullah380@gmail.com>
2024-09-12 19:55:16 +0500

    Files Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'calculator_screen.dart';
 
+// Main function to run the app
 void main() => runApp(MyApp());
 
+// The main widget for the app
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Calculator(),
+      debugShowCheckedModeBanner: false, // Remove debug banner
+      home: Calculator(), // Start with the Calculator screen
     );
   }
 }
